9cad2d6a016598dfece83141cfcc8681d126b9d3
hsiunien <zens53@163.com>
2025-03-25 09:38:41 +0800

    升级编译环境

--- src/androidTest/java/wang/xiunian/android/AndroidLTest.java
+++ src/androidTest/java/wang/xiunian/android/AndroidLTest.java
@@ -1,7 +1,7 @@
 package wang.xiunian.android;
 
-import android.support.test.filters.SmallTest;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.filters.SmallTest;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import junit.framework.Assert;
 
